65a60a343a4d5eb764f0ec296f61a357013304cf
utkalgrouptech <server@utkalsmart.com>
2026-04-29 17:34:33 +0530

    Add service8 import to Home component

--- src/utils/Home/index.tsx
+++ src/utils/Home/index.tsx
@@ -1,4 +1,4 @@
-import { client1, client10, client11, client12, client2, client3, client4, client5, client6, client7, client8, client9, p1, p2, p3, p4, p5, service1, service2, service3, service4, service5, service6, service7, slider1, slider2, slider3, clientraj,shree,arti } from "@/assests/Home";
+import { client1, client10, client11, client12, client2, client3, client4, client5, client6, client7, client8, client9, p1, p2, p3, p4, p5, service1, service2, service3, service4, service5, service6, service7, slider1, slider2, slider3, clientraj,shree,arti,service8 } from "@/assests/Home";
 import { navArrType } from "@/types/index";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
